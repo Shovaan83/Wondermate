0636b7c8c03a1ac341357fa4dfdf3ef80efa250d
Shovan Bhattarai <151829594+Shovaan83@users.noreply.github.com>
2024-07-10 12:05:00 +0545

    Initial commit

---
+++ src/Components/LandingPage.jsx
@@ -0,0 +1,11 @@
+
+const LandingPage = () => {
+  return (
+    <div>
+        <h1>This is the Landing page </h1>
+        <img src="./assets/Beach.jpg" alt="" />
+    </div>
+  )
+}
+
+export default LandingPage
